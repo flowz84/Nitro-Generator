1b2aa02c9e55dd7f303836adb237f284d7cf346d
FlowZ <104300626+flowz84@users.noreply.github.com>
2022-05-06 20:23:45 +0200

    Add files via upload

---
+++ instalate.bat
@@ -0,0 +1,42 @@
+:menu
+@echo off & color 0f & cls & title FlowZ Generator
+mode 1920, 1080
+color D
+echo.                                                                                
+echo                                                                     #######################                  ########################
+echo                                                                     #######################                  #######################
+echo                                                                     #######                                                 #######        
+echo                                                                     #######                                                #######                                                       
+echo                                                                     ################                                      #######
+echo                                                                     ################                                     ####### 
+echo                                                                     #######                                             #######
+echo                                                                     #######                                            ####### 
+echo                                                                     #######                                           ####### 
+echo                                                                     #######                                          #######              
+echo                                                                     #######                                         ####### 
+echo                                                                     #######                                        #######################
+echo                                                                     #######                                       ########################
+echo.                       
+echo.                       
+echo.
+echo.
+echo.
+echo                                                                                         Discord Nitro codes 
+echo.
+echo By : FlowZ                                                                                                                                                                                                                - FZCommunity
+echo.
+echo Generator started at : %date% %time%
+echo.                                                                                                                               
+echo                                                                                   [1] Lancer le generateur
+echo.
+set /p answer="Quelle option choisissez-vous ? >> "
+if /i {%ANSWER%}=={1} (goto :1)
+goto :menu
+
+
+:1
+echo.
+color A
+echo.
+echo [LOG] - [%time%] -  https://discord.gift%random%
+goto :1
